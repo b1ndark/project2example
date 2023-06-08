1309603e3fd1caa470c099e2fb247f565f69002a
b1ndark <>
2023-06-08 19:02:35 +0000

    add functions to medium and hard buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
+
+// buttons to access 
 const startButton = document.getElementById("start-btn");
+const easyButton = document.getElementById("easy-btn");
+const mediumButton = document.getElementById("medium-btn");
+const hardButton = document.getElementById("hard-btn");
+
+// Containers
 const startMenu = document.getElementById("menu-container");
 const difficultyContainerElement = document.getElementById("difficulty-container");
-const easyButton = document.getElementById("easy-btn");
 const questionContainerElement = document.getElementById("question-container");
 
-
+// By pressing Start it will take you to Difficulty Menu
 startButton.addEventListener('click', selectDifficulty);
 
 function selectDifficulty() {
@@ -13,10 +19,31 @@ function selectDifficulty() {
     difficultyContainerElement.classList.remove('hide');
 }
 
+// This function will select Easy mode
 easyButton.addEventListener('click', selectEasy);
 
 function selectEasy() {
     console.log("easy");
+    difficultyContainerElement.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
+}
+
+// This function will select Medium mode
+
+mediumButton.addEventListener('click', selectMedium);
+
+function selectMedium() {
+    console.log("medium");
+    difficultyContainerElement.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
+}
+
+// This function will select Hard mode
+
+hardButton.addEventListener('click', selectHard);
+
+function selectHard() {
+    console.log("hard");
     difficultyContainerElement.classList.add('hide');
     questionContainerElement.classList.remove('hide');
 }

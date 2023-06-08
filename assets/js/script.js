97100647b3e9b8bf72b3ff35fb14f493bbae1a81
b1ndark <>
2023-06-08 00:38:35 +0000

    easy mode button working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
 const startButton = document.getElementById("start-btn");
-const startMenu = document.getElementById("start-menu");
+const startMenu = document.getElementById("menu-container");
+const difficultyContainerElement = document.getElementById("difficulty-container");
+const easyButton = document.getElementById("easy-btn");
+const questionContainerElement = document.getElementById("question-container");
 
 
 startButton.addEventListener('click', selectDifficulty);
 
 function selectDifficulty() {
     console.log('difficulty menu');
+    startMenu.classList.add('hide');
+    difficultyContainerElement.classList.remove('hide');
+}
 
+easyButton.addEventListener('click', selectEasy);
+
+function selectEasy() {
+    console.log("easy");
+    difficultyContainerElement.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
 }

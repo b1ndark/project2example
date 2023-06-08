d939766f0479cc97ce4c7e21e669a54b83dda52f
b1ndark <>
2023-06-08 00:15:39 +0000

    add javascript content and tested it 'start button'

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const startButton = document.getElementById("start-btn");
+const startMenu = document.getElementById("start-menu");
+
+
+startButton.addEventListener('click', selectDifficulty);
+
+function selectDifficulty() {
+    console.log('difficulty menu');
+
+}

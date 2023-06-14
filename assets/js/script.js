f2cbb29f73f7a3ce4168318b1e20f888257881f4
b1ndark <>
2023-06-14 20:31:18 +0000

    add comments to functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 
-// buttons to access its containers and difficulties
+
+/**
+ *  Access to containers
+ */
+
+// Buttons to access its containers
 const startButton = document.getElementById("start-btn");
-const easyButton = document.getElementById("easy-btn");
-const mediumButton = document.getElementById("medium-btn");
-const hardButton = document.getElementById("hard-btn");
 const instructionsButton = document.getElementById("instructions-btn");
 const closeInstructionsButton = document.getElementById("close-instructions-btn");
 const difficultyMenuButton = document.getElementById("difficulty-menu-btn");
+
+// Difficulty Buttons
+const easyButton = document.getElementById("easy-btn");
+const mediumButton = document.getElementById("medium-btn");
+const hardButton = document.getElementById("hard-btn");
+
 
 // Containers
 const startMenu = document.getElementById("menu-container");
@@ -15,18 +23,26 @@ const questionContainerElement = document.getElementById("question-container");
 const instructionsContainerElement = document.getElementById("instructions-container");
 
 
-// By pressing Instructions it will take you to Instructions container
+/**
+ * By pressing Instructions it will take you to Instructions container
+ * Event Listener to select Instructions
+ */
 instructionsButton.addEventListener('click', selectInstructions);
 
+// Function to open the Instructions container
 function selectInstructions() {
     console.log("instructions");
     startMenu.classList.add('hide');
     instructionsContainerElement.classList.remove('hide');
 }
 
-// Closing instructions
+/**
+ * Closing instructions
+ * Event Listener to take back to Main Menu Container
+ */
 closeInstructionsButton.addEventListener('click', selectMainMenu);
 
+// Function to close Instructions
 function selectMainMenu() {
     console.log("close");
     instructionsContainerElement.classList.add('hide');
@@ -42,6 +58,10 @@ function selectDifficulty() {
     startMenu.classList.add('hide');
     difficultyContainerElement.classList.remove('hide');
 }
+
+/**
+ * Variables
+ */
 
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
@@ -62,7 +82,10 @@ function selectEasyQuiz() {
     nextQuestionButton.innerHTML = "Next";
     showEasyQuestion();
 }
-
+/**
+ * This function will reset the answer buttons from its questions
+ * 
+ */
 function resetEasyState() {
     while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
@@ -81,7 +104,7 @@ function showEasyQuestion() {
     questionElement.innerHTML = currentEasyQuestion.question;
 
     /**
-     *  Function is to show answers of the current question
+     *  This Function is to show answers of the current question
      *  It will add a button for each answer of the current question, in this case 4 answers
      *  Data for for the answers will be collected from game.js file
      **/
@@ -98,10 +121,15 @@ function showEasyQuestion() {
     });
 };
 
+/**
+ * This function will activate as soon as the user selects an answer
+ */
+
 function selectEasyAnswer(event) {
     console.log("selected answer");
     const selectedBtn = event.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
+    // Here the function will check if the answer is correct or not
     if (isCorrect) {
         console.log("correct-answer");
         selectedBtn.classList.add("correct-answer");
@@ -109,6 +137,7 @@ function selectEasyAnswer(event) {
         console.log("wrong-answer");
         selectedBtn.classList.add("wrong-answer");
     }
+    // Soon as the answer is selected where true or false, all the answers will be locked
     Array.from(answerButtons.children).forEach(button => {
         if (button.dataset.correct === "true") {
             button.classList.add("correct-answer");
@@ -116,6 +145,7 @@ function selectEasyAnswer(event) {
         button.disabled = true;
         console.log("answers blocked");
     });
+    // Once the answers are selected and locked, the Next button will be displayed
     nextQuestionButton.style.display = "block";
 
 }

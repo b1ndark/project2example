79a6142b6e5c55d2cd9ea9c38c50b102aeadeb9a
b1ndark <>
2023-06-15 14:43:55 +0000

    add hard mode questions

--- assets/js/game.js
+++ assets/js/game.js
@@ -77,3 +77,42 @@ const mediumQuestions = [
         ]
     }
 ];
+
+const hardQuestions = [
+    {
+        questionH: 'When did happen the American civil war?',
+        answers: [
+            { text: 'between 1 Jan 1250 and 31 Dec 1900', correct: false },
+            { text: 'between 31 Feb 2100 and 25 Apr 2135', correct: false },
+            { text: 'between 12 Apr 1861 and 9 Apr 1865', correct: true },
+            { text: 'between 5 Jun 1800 and 20 May 1899', correct: false }
+        ]
+    },
+    {
+        questionH: 'Which Roman emperor built the Colosseum?',
+        answers: [
+            { text: 'emperor Magnus Maximus', correct: false },
+            { text: 'emperor Constantine II', correct: false },
+            { text: 'emperor Vespasian', correct: true },
+            { text: 'emperor Julius Caesar', correct: false }
+        ]
+    },
+    {
+        questionH: 'In Japan where did the USA detonated the two nuclear bombs during the WW2?',
+        answers: [
+            { text: 'Hiroshima and Nagasaki', correct: true },
+            { text: 'Osaka and Tokyo', correct: false },
+            { text: 'Nagoya and Kobe', correct: false },
+            { text: 'Yokohama and Niigata', correct: false }
+        ]
+    },
+    {
+        questionH: 'What was the longest Empire in history?',
+        answers: [
+            { text: 'Kush Empire', correct: false },
+            { text: 'Roman Empire', correct: true },
+            { text: 'Ethiopian Empire', correct: false },
+            { text: 'Khmer Empire', correct: false }
+        ]
+    }
+];

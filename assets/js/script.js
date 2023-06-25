86b1affa9afdbe71e1d2a86f83eedd94ad4fd047
b1ndark <>
2023-06-25 22:25:19 +0000

    add new code from my main project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,119 +1,140 @@
-
-
 /**
-*  Access to containers
+* Buttons to access its containers
+* Global Variables
 */
-
-// Buttons to access its containers
 const startButton = document.getElementById("start-btn");
 const instructionsButton = document.getElementById("instructions-btn");
 const closeInstructionsButton = document.getElementById("close-instructions-btn");
-const backToIndexButton = document.getElementById("back-index-btn");
+const backToIndexButton = document.getElementById("back-to-index-btn");
+const backToDifficultyMenu = document.getElementById("back-to-difficulty-menu");
 
-// Containers 
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const scoreAreaDisplay = document.getElementById("score-area");
+const answeredQuestionsCounter = document.getElementById("answered-question-counter");
+const progressAnsweredQuestionBarFull = document.getElementById("fill-up-progress-question-bar");
+
+
+let currentQuestionIndex = 0;
+let score = 0;
+let currentQuestion = {};
+let difficulty = "";
+
+
+/** 
+* Containers
+*/
 const startMenu = document.getElementById("menu-container");
 const difficultyContainerElement = document.getElementById("difficulty-container");
-const questionContainerElement = document.getElementById("question-container");
 const instructionsContainerElement = document.getElementById("instructions-container");
-
+const questionContainerElement = document.getElementById("question-container");
 
 /**
- * By pressing Instructions it will take you to Instructions container
- * Event Listener to select Instructions
- */
+* Instructions container
+* By selecting Instructions button, you will be taken to Instructions container
+* Event Listener to select Instructions
+*/
 instructionsButton.addEventListener('click', selectInstructions);
 
-// Function to open the Instructions container
+// This function will open the Instructions container
 function selectInstructions() {
-    console.log("instructions");
+    console.log("open instructions");
     startMenu.classList.add('hide');
     instructionsContainerElement.classList.remove('hide');
 }
 
 /**
- * Closing instructions
- * Event Listener to take back to Main Menu Container
- */
+* By pressing Close button, it will close the Instructions container and take you back to Main Menu Container
+* Event Listener to close instructions
+*/
 closeInstructionsButton.addEventListener('click', selectMainMenu);
 
-// Function to close Instructions
+// This function will close the Instructions container
 function selectMainMenu() {
-    console.log("close");
+    console.log("close instructions");
     instructionsContainerElement.classList.add('hide');
     startMenu.classList.remove('hide');
+    console.log('back to Main Menu');
 }
 
 
-// By pressing Start it will take you to Difficulty Menu
+/**
+* By pressing Start it will take you to the Difficulty Menu
+* Event Listener to take back to Difficulty Menu
+*/
 startButton.addEventListener('click', selectDifficulty);
 
+// Function to close Start Menu and open Difficulty Menu
 function selectDifficulty() {
     console.log('difficulty menu');
     startMenu.classList.add('hide');
     difficultyContainerElement.classList.remove('hide');
+    console.log('closed main menu');
+    questionContainerElement.classList.add('hide');
 }
 
 /**
- * Variables
+ * By pressing back button it will take you back to Difficulty Menu
  */
+backToDifficultyMenu.addEventListener('click', selectDifficulty);
 
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const scoreDisplay = document.getElementById("score-area");
-const questionCounterText = document.getElementById("question-counter");
-const scoreCounterText = document.getElementById("score-counter");
-const progressQuestionBarFull = document.getElementById("progress-question-bar-full");
-
-
-let currentQuestionIndex = 0;
-let score = 0;
 
 /**
- * Global functions 
- */
-
-/**
- * This Function will get the current score
- * and increase it by 1 as you progress and select correct answers
- */
+* Love Maths project helped me with this function
+* This Function will get the current score
+* and increase it by 1 as you progress and select correct answers
+*/
 function addCorrectAnswersScore() {
     let previousCorrectAnswersScore = parseInt(document.getElementById("correct-answers-score").innerText);
     document.getElementById("correct-answers-score").innerText = ++previousCorrectAnswersScore;
 }
 
+
+// Difficulty menu selection
 const easy = document.getElementById("easy-btn");
 const medium = document.getElementById("medium-btn");
 const hard = document.getElementById("hard-btn");
 
-let currentQuestion = {};
+/** 
+* There are three modes Easy, Medium and Hard
+* The game will start on the mode that you have selected
+*/
 
-function selectQuiz() {
-    console.log("mode selected");
+function selectQuiz(selectedDifficulty) {
+    console.log('you have selected  mode');
     difficultyContainerElement.classList.add('hide');
     questionContainerElement.classList.remove('hide');
     currentQuestionIndex = 0;
     score = 0;
+    difficulty = selectedDifficulty;
     showQuestion();
 }
+
 /**
-* This function will reset the answer buttons from its questions
-* 
-*/
+    * This function will reset answers from previous questions
+    * 
+    */
 function resetState() {
     while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
+        console.log("previous answers cleared");
     }
 }
 
-//Show questions and answers
-function showQuestion(difficulty) {
+
+/**
+* This function will show questions and its answers
+*/
+
+function showQuestion() {
     resetState();
     console.log("show question");
-    /**
-     * This function will show current question
-     * Data for for the questions will be collected from game.js file
-     **/
 
+    /**
+    * This function will show current question
+    * Data for the questions will be collected from game.js file
+    * It will pick the questions from the mode you have choosen
+    */
     if (difficulty == easy) {
         currentQuestion = easyQuestions[currentQuestionIndex];
         console.log("easy questions");
@@ -128,17 +149,17 @@ function showQuestion(difficulty) {
 
     // This will workout what question you are on and display it
     currentQuestionIndex++;
-    questionCounterText.innerHTML = `${currentQuestionIndex}/4`;
+    answeredQuestionsCounter.innerHTML = `${currentQuestionIndex}/4`;
 
     // This will display a progression bar
-    progressQuestionBarFull.style.width = `${(currentQuestionIndex / 4) * 100}%`;
+    progressAnsweredQuestionBarFull.style.width = `${(currentQuestionIndex / 4) * 100}%`;
+
 
     /**
-     *  This Function is to show answers of the current question
-     *  It will add a button for each answer of the current question, in this case 4 answers
-     *  Data for for the answers will be collected from game.js file
-     **/
-
+    * This function is to show answers of the current question
+    * It will add a button for each answer of the current question, in this case 4 answers
+    * Data for the answers will be collected from game.js file
+    */
     currentQuestion.answers.forEach(answer => {
         console.log("answers displayed");
         const answerButton = document.createElement("button");
@@ -148,53 +169,83 @@ function showQuestion(difficulty) {
         if (answer.correct) {
             answerButton.dataset.correct = answer.correct;
         }
+
+        // This Event Listener is to select an answer
         answerButton.addEventListener('click', selectAnswer);
     });
 }
 
-/**
- * This function will activate as soon as the user selects an answer
- */
 
+/**
+* This function will activate as soon as the User selects an answer
+*/
 function selectAnswer(event) {
-    console.log("selected answer");
-    const selectedBtn = event.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
-    // Here the function will check if the answer is correct or not
-    if (isCorrect) {
-        console.log("correct-answer");
-        selectedBtn.classList.add("correct-answer");
+    console.log(difficulty);
+    console.log("answer selected");
+    const selectedAnswerButton = event.target;
+    const correctAnswer = selectedAnswerButton.dataset.correct === "true";
+
+    /**
+    * The answer will be checked whether is correct or wrong
+    * Also class has been added to decorate/style the correct and wrong answers
+    **/
+    if (correctAnswer) {
+        console.log("its correct");
+        selectedAnswerButton.classList.add("correct-answer");
         score++;
         addCorrectAnswersScore();
     } else {
-        console.log("wrong-answer");
-        selectedBtn.classList.add("wrong-answer");
+        console.log("its wrong");
+        selectedAnswerButton.classList.add("wrong-answer");
     }
-    // Soon as the answer is selected where true or false, all the answers will be locked
+
+    // Soon as the answer is selected whether is correct or wrong, all answers will be locked.
     Array.from(answerButtons.children).forEach(button => {
         if (button.dataset.correct === "true") {
             button.classList.add("correct-answer");
         }
         button.disabled = true;
-        console.log("answers blocked");
+        console.log("answers locked");
     });
+
     // Once answer is selected whether is correct or wrong it will automatically move to the next one
     setTimeout(() => {
         handleNextQuestion();
-    }, 2000);
+    }, 1500);
 }
 
+
+/**
+* This function will show the user score at the end of the quiz.
+* A text message has been added to congratulate the user.
+*/
 function showScore() {
     resetState();
-    questionElement.innerHTML = `You have scored ${score} out of ${4}!`;
-    backToIndexButton.style.display = "block";
-    scoreDisplay.style.display = "none";
+    questionElement.innerHTML = `Well done in completing the quiz!` +
+        `<br> You have answered ${score} correct out of ${4} questions!`;
+
+    // This will display Main Menu button
+    backToIndexButton.style.display = 'block';
+
+    // This will Hide Score Area
+    scoreAreaDisplay.style.display = 'none';
+
+    // This will Hide the back button
+    backToDifficultyMenu.style.display = 'none';
 }
 
+
+/**
+* This function adds next question so the user can carry on with the quiz
+* Next question Data will be loaded from game.js file
+*/
 function handleNextQuestion() {
     if (currentQuestionIndex < 4) {
         showQuestion();
+        console.log("next question shown");
     } else {
         showScore();
-    };
-}
+    }
+};
+
+
